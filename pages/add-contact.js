458ced6d0c1702990e8c092fa40aba350a2c1586
Dalveer Singh <dalveersidhu97@gmail.com>
2022-03-10 15:04:28 -0500

    --

--- pages/add-contact.js
+++ pages/add-contact.js
@@ -1,6 +1,3 @@
-import { useContext, useEffect } from 'react'
-import { useRouter } from "next/router";
-import { LoginContext } from '../store/LoginContext'
 import AddContact from '../Components/AddContact';
 
 export default function AddContactPage() {
